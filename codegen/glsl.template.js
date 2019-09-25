eebc0e0e6445973be318c931a44eb5df67919804
Sylvain Meunier <sylvain.meunier@gmail.com>
2019-09-25 22:26:56 +0200

    Add dual sphere shortcut

--- codegen/glsl.template.js
+++ codegen/glsl.template.js
@@ -1,20 +1,38 @@
 
 // GLSL template for our struct and our constructors
-var preamble = (basis,classname) =>
+var preamble = (basis, classname) =>
 `// Generator written by meuns.
 
 struct ${classname} {  
-    float mvec[${basis.length}];
+  float mvec[${basis.length}];
 };
 
 //***********************
-//
-//
+// ${classname}.Zero : res = 0
+// Fill the multivector with 0.
 //***********************
 ${classname} Zero() {
   ${classname} res;
-  // TODO
+  ${[...Array(basis.length)].map((x,i)=>"  res.mvec[" + i + "] = 0.0;").join("\n")}
   return res;
+}
+
+//***********************
+// ${classname}.Set : res[i] = v
+// Set blade value.
+//***********************
+${classname} Set(in CGA a, int i, float v) {
+  ${classname} res = a;
+  res.mvec[i] = v;
+  return res;
+}
+
+//***********************
+// ${classname}.Get : v = a[i]
+// Get blade value.
+//***********************
+float Get(in CGA a, int i) {
+  return a.mvec[0];
 }`
 
 // GLSL template for our binary operators
@@ -43,6 +61,26 @@ ${classname} ${name}(in ${classname_a} ${name_a}) {
 
 // GLSL template for our postamble
 var postamble = (basis, classname, example) =>
-``;
+`// Setup CGA basis
+const CGA e1 = CGA(float[](+0.0, +1.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0));
+const CGA e2 = CGA(float[](+0.0, +0.0, +1.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0));
+const CGA e3 = CGA(float[](+0.0, +0.0, +0.0, +1.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0));
+const CGA e4 = CGA(float[](+0.0, +0.0, +0.0, +0.0, +1.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0));
+const CGA e5 = CGA(float[](+0.0, +0.0, +0.0, +0.0, +0.0, +1.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0));
+const CGA eo = CGA(float[](+0.0, +0.0, +0.0, +0.0, +1.0, +1.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0));
+const CGA ei = CGA(float[](+0.0, +0.0, +0.0, +0.0, -0.5, +0.5, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0));
 
-Object.assign(exports,{preamble, postamble, unary,binary,desc:"glsl"});
+// Various shortcuts
+float Distance(in CGA a, in CGA b) {
+  return sqrt(-Get(Dot(a, b), 0) * 2.0);
+}
+
+CGA MakePoint(vec3 p) {
+  return Add(smul(p.x, e1), Add(smul(p.y, e2), Add(smul(p.z, e3), Add(smul(0.5 * dot(p, p), ei), eo))));
+}
+
+CGA MakeDualSphere(vec3 c, float r) {
+  return Add(smul(c.x, e1), Add(smul(c.y, e2), Add(smul(c.z, e3), smul(0.5 * r * r, ei))));
+}`;
+
+Object.assign(exports, {preamble, postamble, unary, binary, desc: "glsl"});
