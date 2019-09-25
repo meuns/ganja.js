0a62322fd3e8b9f59869de5a657a36e5b2151858
Sylvain Meunier <sylvain.meunier@gmail.com>
2019-09-25 22:30:31 +0200

    Overload Mul with smul and muls

--- codegen/glsl.template.js
+++ codegen/glsl.template.js
@@ -36,12 +36,13 @@ float Get(in CGA a, int i) {
 }`
 
 // GLSL template for our binary operators
+var overload_mul = (name) => name == "smul" || name == "muls" ? "Mul" : name;
 var binary = (classname, symbol, name, name_a, name_b, name_ret, code, classname_a=classname, classname_b=classname, desc) =>
 `//***********************
 // ${classname}.${name} : ${name_ret} = ${symbol?name_a+" "+symbol+" "+name_b:name_a+"."+name+"("+name_b+")"} 
 // ${desc}
 //***********************
-${classname} ${name}(in ${classname_a} ${name_a}, in ${classname_b} ${name_b}) {
+${classname} ${overload_mul(name)}(in ${classname_a} ${name_a}, in ${classname_b} ${name_b}) {
   ${classname} ${name_ret};
   ${code.replace(/\[/g,'.mvec[')}
   return ${name_ret};
