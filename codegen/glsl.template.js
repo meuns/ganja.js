4a4ffc1eafccf6e74e545952b17ceeef63b36545
Sylvain Meunier <sylvain.meunier@gmail.com>
2019-09-25 22:34:33 +0200

    prealpha shadertoy

--- codegen/glsl.template.js
+++ codegen/glsl.template.js
@@ -85,3 +85,65 @@ CGA MakeDualSphere(vec3 c, float r) {
 }`;
 
 Object.assign(exports, {preamble, postamble, unary, binary, desc: "glsl"});
+
+/*
+const int MAX_MARCHING_STEPS = 1000;
+const float MIN_DIST = 0.0;
+const float MAX_DIST = 100.0;
+const float STEP_SIZE = 0.005;
+const float EPSILON = 0.0001;
+
+CGA MakeDualSphere(vec3 c, float r) {
+    return Add(smul(c.x, e1), Add(smul(c.y, e2), Add(smul(c.z, e3), smul(0.5 * r * r, ei))));
+}
+
+float Sphere(vec3 s) {
+    return Norm(Wedge(MakePoint(s), MakeDualSphere(vec3(0.0, 0.0, 0.0), 1.0)));
+}
+
+float SphereSDF(vec3 s) {
+    return Distance(MakePoint(s), MakePoint(vec3(0.0, 0.0, 0.0))) - 1.0;
+}
+
+float SceneSDF(vec3 samplePoint) {
+    return SphereSDF(samplePoint);
+}
+
+float FirstRoot(vec3 eye, vec3 dir, float start, float end) {
+    float depth = start;
+    for (int i = 0; i < MAX_MARCHING_STEPS; i++) {
+        float dist = SceneSDF(eye + depth * dir);
+        if (dist < EPSILON) {
+			return depth;
+        }
+        depth += STEP_SIZE;
+        if (depth >= end) {
+            return end;
+        }
+    }
+    return end;
+}
+
+vec3 rayDirection(float fieldOfView, vec2 size, vec2 fragCoord) {
+    vec2 xy = fragCoord - size / 2.0;
+    float z = size.y / tan(radians(fieldOfView) / 2.0);
+    return normalize(vec3(xy, -z));
+}
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord)
+{
+	vec3 dir = rayDirection(45.0, iResolution.xy, fragCoord);
+    vec3 eye = vec3(0.0, 0.0, 5.0);
+    float dist = FirstRoot(eye, dir, MIN_DIST, MAX_DIST);
+    
+    if (dist > MAX_DIST - EPSILON) {
+        fragColor = vec4(0.0, 0.0, 0.0, 0.0);
+		return;
+    }
+    
+    // The closest point on the surface to the eyepoint along the view ray
+    vec3 p = eye + dist * dir;    
+    
+    fragColor = vec4(vec3(Sphere(p)), 1.0);
+}
+*/
